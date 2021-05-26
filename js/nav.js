4268218e713740999912bcd9e6f0166c9c3720bc
Akira <mhcheng@anjet.com>
2021-05-26 15:54:00 +0800

    up

--- js/nav.js
+++ js/nav.js
@@ -77,14 +77,3 @@
           $(location.hash).addClass("active");
           location.hash = "";//更改#標籤後方字串
        });
-/*footer_nav*/
-       $(".footer_nav > .level_1 > li > a").click(function() {
-        //抓取網址列最後面的#部分 然後換頁後自動移到該位置
-        $(function () {
-          var gethash = location.hash+ "_area";
-          $("html, body").animate({scrollTop: $(gethash).offset().top-97 }, {duration: 10,easing: "swing"});
-          $(".header_nav  a").removeClass("active");
-          $(location.hash).addClass("active");
-          location.hash = "";//更改#標籤後方字串
-        });
-      })
