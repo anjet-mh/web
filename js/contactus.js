579d8160be00951d9097fa5e28c628a1db247447
Akira <mhcheng@anjet.com>
2021-05-27 18:44:48 +0800

    Update contactus.js

--- js/contactus.js
+++ js/contactus.js
@@ -30,7 +30,7 @@ $("#submit").click(function(){
           To : 'service.tw@anjet.com',
           From : email,
           Subject : "來自【聯絡我們】的通知",
-          Body : "姓名：" & name & "<br>公司名稱跟職務：" & company & "<br>E-mail：" & email & "<br>詢問內容：" & msg,
+          Body : "姓名：" + name + "<br>公司名稱跟職務：" + company + "<br>E-mail：" + email + "<br>詢問內容：" + msg,
       }).then(
         message => alert('詢問的內容 提交完成') & $('#form1')[0].reset()/*送出後清空*/ & $(".Sending").hide(),
       );
