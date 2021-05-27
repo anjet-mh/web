678deb4345af46b250f8a2245d1911e0030fc982
Akira <mhcheng@anjet.com>
2021-05-27 16:40:46 +0800

    UP

--- js/contactus.js
+++ js/contactus.js
@@ -1,3 +1,4 @@
+ $(".Sending").hide(); /**/
 $("#submit").click(function(){
 
     var strEmail = $("#email").val();
@@ -16,14 +17,14 @@ $("#submit").click(function(){
       alert("請輸入完整的E-mail");
       eval("document.form1['email'].focus()");
     }else if($("#textarea").val()==""){
-      alert("請輸入查詢的內容");
+      alert("請輸入詢問的內容");
       eval("document.form1['textarea'].focus()");   
     }else{
+      $(".Sending").show();
       var name = $('#name').val();
       var company = $('#company').val();
       var email = $('#email').val();
       var msg = $('#textarea').val();
-      alert('查詢的內容 傳送中');
       Email.send({
           SecureToken : "2fb0cb33-c0d0-4868-9228-0f622b351d80",
           To : 'service.tw@anjet.com',
@@ -31,7 +32,7 @@ $("#submit").click(function(){
           Subject : "來自【聯絡我們】的通知",
           Body : "姓名：" & name & "<br>公司名稱跟職務：" & company & "<br>E-mail：" & email & "<br>詢問內容：" & msg,
       }).then(
-        message => alert('查詢的內容 傳送完成') & $('#form1')[0].reset()/*送出後清空*/,
+        message => alert('詢問的內容 提交完成') & $('#form1')[0].reset()/*送出後清空*/ & $(".Sending").hide(),
       );
 
       /*GOOGLE表單用 document.form1.submit();*/
