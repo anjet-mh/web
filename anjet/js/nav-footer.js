c29e9323743472887251429f87be818b6e43cd97
anjet-mh <79448729+anjet-mh@users.noreply.github.com>
2021-07-07 15:10:25 +0800

    UP

--- anjet/js/nav-footer.js
+++ anjet/js/nav-footer.js
@@ -1,19 +1,10 @@
-$(".footer_nav > .level_1 > li > a").click(function() {
-  var scrolltarget = "#" + $(this).attr('id') + "_area";
-  alert(scrolltarget);
-$("html, body").animate({scrollTop: $(scrolltarget).offset().top-97 }, {duration: 10,easing: "swing"});        
-$(".header_nav").addClass("header_nav_Open_Close");
-var tagactive = "#" + $(this).attr('id');
-$(".header_nav  a").removeClass("active");
-$(tagactive).addClass("active");
-return false;
-});
-//抓取網址列最後面的#部分 然後換頁後自動移到該位置
-$(function () {
-  var gethash = location.hash+ "_area";
-
-  $("html, body").animate({scrollTop: $(gethash).offset().top-97 }, {duration: 10,easing: "swing"});
-  $(".header_nav  a").removeClass("active");
-  $(location.hash).addClass("active");
-  location.hash = "";//更改#標籤後方字串
-});
+       $(".footer_nav > .level_1 > li > a").click(function() {
+        //抓取網址列最後面的#部分 然後換頁後自動移到該位置
+        $(function () {
+          var gethash = location.hash+ "_area";
+          $("html, body").animate({scrollTop: $(gethash).offset().top-97 }, {duration: 10,easing: "swing"});
+          $(".header_nav  a").removeClass("active");
+          $(location.hash).addClass("active");
+          location.hash = "";//更改#標籤後方字串
+        });
+      })
