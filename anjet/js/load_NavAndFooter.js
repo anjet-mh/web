5be0684c37eecb9b1ca2d42a8d4c85e172a95470
anjet-mh <79448729+anjet-mh@users.noreply.github.com>
2022-04-01 14:51:56 +0800

    UP

---
+++ anjet/js/load_NavAndFooter.js
@@ -0,0 +1,4 @@
+var load_nav = "nav.html?timestamp=" + new Date().getTime();
+var load_footer = "footer.html?timestamp=" + new Date().getTime();
+$("#load-nav").load(load_nav);
+$("#load-footer").load(load_footer);
